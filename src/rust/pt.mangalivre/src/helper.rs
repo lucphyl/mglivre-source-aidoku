c9d6fddb5f8c564eb5423c786683d24e15436780
lucphyl <107371429+lucphyl@users.noreply.github.com>
2026-05-06 21:46:23 -0300

    Update helper.rs
    alterado para mangalivre

--- src/rust/pt.mangalivre/src/helper.rs
+++ src/rust/pt.mangalivre/src/helper.rs
@@ -2,45 +2,40 @@ use aidoku::{error::Result, prelude::format, std::String};
 
 use crate::BASE_URL;
 
-/// Returns the ID of a manga from a URL.
+/// Retorna o ID do mangá a partir da URL.
+///
+/// Exemplo:
+/// https://mangalivre.to/manga/solo-leveling/
+/// -> solo-leveling
 pub fn get_manga_id(url: &str) -> String {
-	// Two Formats
-	// https://demonicscans.org/title/Overgeared/chapter/1/2024090306
-	// https://demonicscans.org/manga/Overgeared
-	// For the chapter format it seems as if the ending part is <year><month><day><hour> where hour is 12hr time in UTC
+	let clean = url.trim_end_matches('/');
 
-	let id_with_suffix = url
-		.split("/manga/")
-		.last()
-		.unwrap_or(url.split("/title/").last().unwrap_or(""));
-
-	// Handle chapter suffix
-	let id_without_chapter_suffix = if let Some(index) = id_with_suffix.rfind("/chapter/") {
-		&id_with_suffix[..index]
-	} else {
-		id_with_suffix
-	};
-
-	String::from(id_without_chapter_suffix)
-}
-
-/// Returns the ID of a chapter from a URL.
-pub fn get_chapter_id(url: &str) -> String {
-	// Example Url: https://demonicscans.org/chaptered.php?manga=4&chapter=1
-	// Example Url: /chaptered.php?manga=4&chapter=1
-	// parse "/chaptered.php?manga=4&chapter=1" from the url
-
-	match url.find("/chaptered.php") {
-		Some(i) => url[i..].into(),
+	match clean.split("/manga/").last() {
+		Some(id) => String::from(id),
 		None => String::from(""),
 	}
 }
 
-/// Returns full URL of a manga from a manga ID.
-pub fn get_manga_url(manga_id: &String) -> String {
-	format!("{}/manga/{}", BASE_URL, manga_id)
+/// Retorna o ID do capítulo.
+///
+/// Exemplo:
+/// https://mangalivre.to/manga/solo-leveling/capitulo-1/
+/// -> /manga/solo-leveling/capitulo-1/
+pub fn get_chapter_id(url: &str) -> String {
+	let clean = url.trim_end_matches('/');
+
+	match clean.split(BASE_URL).last() {
+		Some(path) => format!("{}/", path),
+		None => String::from(""),
+	}
 }
 
+/// Retorna URL completa do mangá.
+pub fn get_manga_url(manga_id: &String) -> String {
+	format!("{}/manga/{}/", BASE_URL, manga_id)
+}
+
+/// Retorna URL completa do capítulo.
 pub fn get_chapter_url(chapter_id: &String) -> Result<String> {
 	Ok(format!("{}{}", BASE_URL, chapter_id))
 }
